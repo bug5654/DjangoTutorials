6bcd5f0cf704f61ba4f72f57c4a9870394c66e84
dbugger <bug.new@gmail.com>
2018-09-20 12:14:42 -0700

    basic tests added

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+	def test_was_published_recently_with_future_question(self):
+		"""was_published_recently should return false for questions with future pub_dates"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+
+		self.assertIs(future_question.was_published_recently(), False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""was_published_recently should return false for questions with pub_date older than 1 day"""
+		time = timezone.now() - datetime.timedelta(days=1,seconds=1)
+		old_question = Question(pub_date=time)
+		self.assertIs(old_question.was_published_recently(),False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""was_published_recently should return True for questions with pub_date within 1 day"""
+		time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+		recent_question = Question(pub_date=time)
+		self.assertIs(recent_question.was_published_recently(), True)
+
+	def notatest(self):
+		print("notatest called!")
+		self.assertIs(True, True)	#just to see if it gets called
+		#does not get called
+
+	def testnotatest(self):
+		print("testnotatest called!")
+		self.assertIs(True, True)	#just to see if it gets called
+		#does get called, so looks for object.test*() to run
